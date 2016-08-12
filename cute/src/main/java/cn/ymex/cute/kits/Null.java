6cfeb9c6f70969e96c9faa64419709ed3a57a278
王乐朋 <ymex@foxmail.com>
2016-08-12 18:16:48 +0800

    Null

--- cute/src/main/java/cn/ymex/cute/kits/Null.java
+++ cute/src/main/java/cn/ymex/cute/kits/Null.java
@@ -1,7 +1,51 @@
 package cn.ymex.cute.kits;
 
 /**
+ * null 处理
  * Created by ymexc on 2016/8/12.
  */
 public class Null {
+    /**
+     * 检查对象是否为空，为空抛出 NullPointerException
+     *
+     * @param notice 提示信息
+     * @param t
+     * @param <T>
+     * @return
+     */
+    public static   <T> T checkNull(T t, String notice) {
+        if (null == t) {
+            if (notice == null) {
+                throw new NullPointerException();
+            }
+            throw new NullPointerException(notice);
+        }
+        return t;
+    }
+
+    /**
+     * 检查对象是否为空，为空抛出 NullPointerException
+     *
+     * @param t
+     * @param <T>
+     * @return
+     */
+    public static  <T> T checkNull(T t) {
+        return checkNull(t, null);
+    }
+
+    /**
+     * 判断对象是否为空
+     *
+     * @param t
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean isNull(T t) {
+        if (null == t) {
+            return true;
+        }
+        return false;
+    }
+
 }
