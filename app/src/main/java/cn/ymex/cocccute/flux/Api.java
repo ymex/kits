d86b6c940ddecdddf5cd6391789a1389da7e5080
王乐朋 <ymex@foxmail.com>
2016-10-27 17:35:13 +0800

    weather request

---
+++ app/src/main/java/cn/ymex/cocccute/flux/Api.java
@@ -0,0 +1,13 @@
+package cn.ymex.cocccute.flux;
+
+/**
+ * Copyright (c) ymexc(www.ymex.cn)
+ * Email:ymex@foxmail.com
+ * date 2016/10/27
+ *
+ * @author ymexc
+ */
+public interface Api {
+
+    String url = "http://www.weather.com.cn/data/cityinfo/101220101.html";
+}
