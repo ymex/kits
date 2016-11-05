24741f82f9655e4ebc302c09673ec8c90d129929
ymex <ymex@foxmail.com>
2016-11-05 20:07:54 +0800

    opt code

--- app/src/main/java/cn/ymex/cocccute/activity/MainActivity.java
+++ app/src/main/java/cn/ymex/cocccute/activity/MainActivity.java
@@ -43,36 +43,37 @@ public class MainActivity extends BaseActivity implements AdapterView.OnItemClic
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-        switch (position) {
-
-            case 1:
-                toastClickCount++;
-                Toaster.show("点击:" + adapter.getItem(position).getTitle() + " " + toastClickCount + "次");
-                break;
-            case 2:
-                Toaster.show(Toaster.inflate(R.layout.view_toast));
-                break;
-            case 3:
-                L.v("this is v log");
-                L.i(new Student("ymex", 12, "hello world !"));
-                L.d(new Student("Jay", 32, "Hi mars"));
-                L.w(new Gson().toJson(new Student("Mars", 32, "你好")));
-                L.e("this error log ");
-                L.w(new Exception("--ht"));
-                break;
-            default:
-                items.get(position).startActivity(this);
-                break;
+        showToast(position);
+        if (position == 3) {
+            showLog();
         }
+        items.get(position).startActivity(this);
+    }
+
+    private void showToast(int position) {
+        if (position == 1) {
+            toastClickCount++;
+            Toaster.show("点击:" + adapter.getItem(position).getTitle() + " " + toastClickCount + "次");
+        } else if (position == 2) {
+            Toaster.show(Toaster.inflate(R.layout.view_toast));
+        }
+    }
+
+    private void showLog() {
+        L.v("this is v log");
+        L.i(new Student("ymex", 12, "hello world !"));
+        L.d(new Student("Jay", 32, "Hi mars"));
+        L.w(new Gson().toJson(new Student("Mars", 32, "你好")));
+        L.e("this error log ");
+        L.w(new Exception("--ht"));
     }
 
     List<ItemEntity> items = new ArrayList() {
         {
             add(new ItemEntity("Flux For Android", "使用Flux模式设计App", FluxActivity.class));
-            add(new ItemEntity("默认Toast", "Toast多次弹出，只显示最后一条"));
-            add(new ItemEntity("定制Toast", "自定义Toast布局，只显示最后一条"));
-            add(new ItemEntity("Log打印", "举个栗子,在Logcat查看"));
+            add(new ItemEntity("默认Toast", "Toast多次弹出，只显示最后一条"));//1
+            add(new ItemEntity("定制Toast", "自定义Toast布局，只显示最后一条"));//2
+            add(new ItemEntity("Log打印", "举个栗子,在Logcat查看"));//3
             add(new ItemEntity("LoadingView", "view for loading ", LoadingActivity.class));
             add(new ItemEntity("RecyclerView", "click , header,footer", RecyclerViewActivity.class));
             add(new ItemEntity("RefreshLayout", "swipeLayout", RefreshLayoutActivity.class));
