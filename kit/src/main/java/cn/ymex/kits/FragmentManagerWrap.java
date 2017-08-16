358d40b3b7ad3361f09eec8876ef169f6d5a035d
王乐朋 <ymex@foxmail.com>
2017-08-16 17:51:16 +0800

    fragmentmanage

--- kit/src/main/java/cn/ymex/kits/FragmentManagerWrap.java
+++ kit/src/main/java/cn/ymex/kits/FragmentManagerWrap.java
@@ -22,10 +22,24 @@ public class FragmentManagerWrap {
         this.fragmentManager = manager;
     }
 
-    public static FragmentManagerWrap  build(FragmentManager manager) {
+    public static FragmentManagerWrap build(FragmentManager manager) {
         return new FragmentManagerWrap(manager);
     }
 
+
+    /**
+     * 是否已经存在依附的fragment
+     * @return
+     */
+    public boolean attached() {
+        return this.getFragmentsSize() > 0;
+    }
+
+    /**
+     * 添加新的fragment 到FragmentManagerWrap中
+     * @param fragments
+     * @return
+     */
     public FragmentManagerWrap add(Fragment... fragments) {
         if (fragments.length <= 0) {
             return this;
@@ -41,16 +55,32 @@ public class FragmentManagerWrap {
         }
         return this;
     }
+
+    public void restore() {
+        if (fragmentManager == null) {
+            throw new IllegalArgumentException("fragmentManager is null");
+        }
+        add(fragmentManager.getFragments().toArray(new Fragment[fragmentManager.getFragments().size()]));
+
+    }
+
+    public int getFragmentsSize() {
+        return fragmentManager.getFragments().size();
+    }
+
     /**
      * 切换显示指定fragment 序列，添加顺序
+     *
      * @param index
      */
-    public void showFragment( int index) {
+    public void showFragment(int index) {
+
         this.showFragment(getFragments().get(index));
     }
 
     /**
      * 切换显示指定fragment
+     *
      * @param fragment
      */
     public void showFragment(Fragment fragment) {
@@ -66,10 +96,11 @@ public class FragmentManagerWrap {
     }
 
     /**
-     * 添加到view中，默认显示第一个Fragment
+     * 把fragment 依附于view 并显示列表中第一个Fragment
+     *
      * @param containerViewId
      */
-    public void attach( @IdRes int containerViewId) {
+    public void attach(@IdRes int containerViewId) {
         if (getFragments().size() <= 0) {
             throw new IllegalArgumentException("Fragment list is null");
         }
@@ -81,10 +112,14 @@ public class FragmentManagerWrap {
     }
 
     public Fragment getFirst() {
-        return getFragments().size()>0?getFragments().get(0):null;
+        return getFragments().size() > 0 ? getFragments().get(0) : null;
     }
 
-    public List<Fragment> getFragments() {
+    private List<Fragment> getFragments() {
         return mFragments == null ? mFragments = new ArrayList<>(4) : mFragments;
+    }
+
+    public FragmentManager getFragmentManager() {
+        return fragmentManager;
     }
 }
